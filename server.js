1cbafd7a6aab309b3786714c0538d438a36ecfc0
jamestang12 <homing.tang@ryerson.ca>
2020-04-07 02:19:18 -0400

    Created Model

--- server.js
+++ server.js
@@ -1,19 +1,40 @@
 const express = require('express');
 const dotenv = require('dotenv');
-
-//Route files
-const bootcapms = require('./routes/bootcamps.js');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 //Load env vars
 dotenv.config({path: './config/config.env'});
 
+//Route files
+const bootcapms = require('./routes/bootcamps.js');
+
+//Connect to database
+connectDB();
+
+
 const app = express();
+
+
+//Dev loggin middleware
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
 
 //Mount routes
 app.use('/api/v1/bootcamps' , bootcapms);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow));
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}.red`);
+    //Close server & exit process
+    server.close(() => process.exit(1));
+    
+})
 
 
