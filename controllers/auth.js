24f8c94f450ca84bc6cb95ba230d054ff7068713
jamestang12 <homing.tang@ryerson.ca>
2020-04-11 19:12:40 -0400

    User Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,7 +3,7 @@ const User = require('../models/User');
 const asyncHandler = require('../middleware/async');
 
 //@desc    Register user
-//@route   GET /api/v1/auth/register
+//@route   POST /api/v1/auth/register
 //@access  Public
 exports.register = asyncHandler(async (req, res, next) => {
     const { name, email, password, role } = req.body;
@@ -15,6 +15,37 @@ exports.register = asyncHandler(async (req, res, next) => {
         password,
         role
     });
+
+    //Create token
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({success: true, token});
+})
+
+//@desc    Login user
+//@route   POST /api/v1/auth/login
+//@access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+    const {email, password } = req.body;
+
+    //Validate email & password
+    if(!email || !password){
+        return next(new ErrorResponse('Please provid an email and password', 400));
+    }
+
+    //check for user
+    const user = await User.findOne({email}).select('+password');
+
+    if(!user){
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
+
+    //Check if password matches
+    const isMatch = await user.matchPassword(password);
+
+    if(!isMatch){
+        return next(new ErrorResponse('Invalid credentials', 401));
+    }
 
     //Create token
     const token = user.getSignedJwtToken();
--- models/User.js
+++ models/User.js
@@ -46,6 +46,11 @@ UserSchema.methods.getSignedJwtToken = function(){
     return jwt.sign({id: this._id}, process.env.JWT_SECRET, {
         expiresIn: process.env.JWT_EXPIRE
     });
+};
+
+//Match user entered password to hashed password in database
+UserSchema.methods.matchPassword = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
 }
 
 module.exports = mongoose.model('User', UserSchema);
