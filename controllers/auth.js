3adc59dff5860caa5a3151bb7f46aef0101c5b69
jamestang12 <homing.tang@ryerson.ca>
2020-04-13 20:52:20 -0400

    Update Password

--- controllers/auth.js
+++ controllers/auth.js
@@ -62,6 +62,44 @@ exports.getMe = asyncHandler(async(req, res, next) =>{
     })
 })
 
+//@desc    Update user details
+//@route   PUT /api/v1/auth/updatedetails
+//@access  Prive
+exports.updateDetails = asyncHandler(async(req, res, next) =>{
+    const fieldsToUpdate={
+        name: req.body.name,
+        email: req.body.email
+    }
+    const user = await User.findByIdAndUpdate(req.user.id, fieldsToUpdate,{
+        new: true,
+        runValidators: true
+    });
+
+    res.status(200).json({
+        success: true,
+        data: user
+    })
+})
+
+//@desc    Update password
+//@route   PUT /api/v1/auth/updatepassword
+//@access  Prive
+exports.updatePassword = asyncHandler(async(req, res, next) =>{
+    const user = await User.findById(req.user.id).select('+password');
+
+    //Check current password
+    if(!(await user.matchPassword(req.body.currentPassword))){
+        return next(new ErrorResponse('Password is incorrent', 401));
+    }
+
+    user.password = req.body.newPassword;
+    await user.save();
+
+    sendTokenRosponse(user, 200, res);
+})
+
+
+
 //@desc    Forgot password
 //@route   POST /api/v1/auth/forgotpassword
 //@access  Public
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { register, login, getMe, forgotPassword, resetPassword } = require('../controllers/auth');
+const { register, login, getMe, forgotPassword, resetPassword, updateDetails, updatePassword} = require('../controllers/auth');
 
 const router = express.Router();
 const { protect } = require('../middleware/auth')
@@ -9,6 +9,8 @@ router.post('/login',login);
 router.get('/me',protect, getMe);
 router.post('/forgotpassword', forgotPassword);
 router.put('/resetpassword/:resettoken', resetPassword);
+router.put('/updatedetails',protect, updateDetails);
+router.put('/updatepassword', protect, updatePassword);
 
 
 
