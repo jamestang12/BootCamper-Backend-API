f869993b0f4e80b475c9347b159abb4d2bcb31d5
jamestang12 <homing.tang@ryerson.ca>
2020-04-14 22:58:28 -0400

    PUT & DELETE REVIEW

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
-const {getReviews, getReview, addReview} = require('../controllers/reviews');
+const {getReviews, getReview, addReview, updateReview, deleteReview} = require('../controllers/reviews');
 
 const Review = require('../models/Review');
 const advancedResults = require('../middleware/advancedResult');
@@ -13,7 +13,7 @@ router.route('/').get(advancedResults(Review,{
     select: 'name description'
 } ) ,getReviews).post(protect, authorize('user', 'admin') ,addReview);
 
-router.route('/:id').get(getReview);
+router.route('/:id').get(getReview).put(protect, authorize('user', 'admin'), updateReview).delete(protect, authorize('user' , 'admin'), deleteReview);
 
 
 
